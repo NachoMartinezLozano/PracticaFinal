969e78cbeb58695a0090bf47cc9f7ed6b6c30c80
NachoMartinezLozano <joseignacio.martinezl@um.es>
2025-06-30 08:53:54 +0200

    Ajustar bien texto a pantalla

---
+++ Front-end/PracticaFinal/src/components/ContenedorPaginaPrincipal.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+
+    <div class="flex flex-col">
+        <h1>Bienvenido a tu Gestor Datastorm</h1>
+        <p>Desde aquí podrás gestionar todos los trabajdores de tu empresa, así como saber en qué están trabajando en todo momento</p>
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
--- Front-end/PracticaFinal/src/pages/PaginaPrincipal.vue
+++ Front-end/PracticaFinal/src/pages/PaginaPrincipal.vue
@@ -2,6 +2,7 @@
     import { ref, onMounted, onUnmounted} from 'vue'
     import Cabecera from '../components/Cabecera.vue'
     import Navegador from '../components/Navegador.vue'
+    import ContenedorPaginaPrincipal from '../components/ContenedorPaginaPrincipal.vue'
 
     const showNavegador = ref(window.innerWidth >= 768)
 
@@ -21,8 +22,12 @@
 <template>
     <div class="flex flex-col min-h-screen">
         <Cabecera />
-        <div class="flex flex-1">
+        <div class="flex flex-row">
             <Navegador v-if="showNavegador" />
+            <main :class="['flex-1 p-4', showNavegador ? 'ml-45' : 'ml-0']">
+                <h1>Bienvenido a tu gestor DataStorm!</h1>
+            </main>
+            
         </div>
     </div>
 
